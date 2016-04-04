f9f9291b3eac0e596fda3f0d59c2ceda75c54602
Toan Nguyen <toannm3110@gmail.com>
2016-04-04 23:05:24 +0700

    add test

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,8 @@ class Item < ActiveRecord::Base
   belongs_to :category
   has_many :reviews, dependent: :destroy
   has_and_belongs_to_many :carts
+
+  validates :title, presence: true
+  validates :title, uniqueness: true
+  validates_associated :category
 end
